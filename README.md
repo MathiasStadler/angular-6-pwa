1ad12a6964ca727683e70c9c7a06777065709566
Mathias Stadler <github@mathias-stadler.de>
2018-09-17 13:41:33 +0200

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# follow this
+
+```chat-angular-websocket-material``
+
+```https://medium.com/dailyjs/real-time-apps-with-typescript-integrating-web-sockets-node-angular-e2b57cbd1ec1'
+
+```
---
+++ server/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var message_1 = require("./model/message");
+exports.Message = message_1.Message;
+var chat_message_1 = require("./model/chat-message");
+exports.ChatMessage = chat_message_1.ChatMessage;
+var user_1 = require("./model/user");
+exports.User = user_1.User;
